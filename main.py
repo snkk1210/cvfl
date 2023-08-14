f38cc8fd108ca368a770cc9eb44ca2c1a3985986
snkk1210 <n41210guitar@gmail.com>
2023-08-14 17:10:16 +0000

    tmp3

--- main.py
+++ main.py
@@ -11,52 +11,7 @@ app = Flask(__name__)
 def lambda_handler(event=None, context=None):
 	return render_template('layout.html')
 
-@app.route('/certificate-upload', methods=['POST'])
-def crtupload():
-    if 'certificate' not in flask.request.files:
-        return render_template('layout.html', message="ファイル未指定です")
-    # ファイルの保存
-    fs = flask.request.files['certificate']
-    fs.save('/tmp/certificate.crt')
-    
-    # 証明書が空であるか判定
-    crtsize = os.path.getsize('/tmp/certificate.crt')
-    if crtsize == 0:
-      return render_template('layout.html', message="＊証明書を選択してアップロードしてください")
-    else:
-      return render_template('layout.html', message="＊証明書のアップロードが完了しました")
-
-@app.route('/privatekey-upload', methods=['POST'])
-def keyupload():
-    if 'privatekey' not in flask.request.files:
-        return render_template('layout.html', message="ファイル未指定です")
-    # ファイルの保存
-    fs = flask.request.files['privatekey']
-    fs.save('/tmp/privatekey.key')
-    
-    # 秘密鍵が空であるか判定
-    keysize = os.path.getsize('/tmp/privatekey.key')
-    if keysize == 0:
-      return render_template('layout.html', message="＊秘密鍵を選択してアップロードしてください")
-    else:
-      return render_template('layout.html', message="＊秘密鍵のアップロードが完了しました")
-
-@app.route('/ca-upload', methods=['POST'])
-def caupload():
-    if 'intermediate' not in flask.request.files:
-        return render_template('layout.html', message="ファイル未指定です")
-    # ファイルの保存
-    fs = flask.request.files['intermediate']
-    fs.save('/tmp/intermediate.ca')
-
-    # 中間証明書が空であるか判定
-    casize = os.path.getsize('/tmp/intermediate.ca')
-    if casize == 0:
-      return render_template('layout.html', message="＊中間証明書を選択してアップロードしてください")
-    else:
-      return render_template('layout.html', message="＊中間証明書のアップロードが完了しました")
-
-@app.route('/exec-script', methods=['POST'])
+@app.route('/exec', methods=['POST'])
 def exec():
   
 	certificate_fs = flask.request.files['certificate']
