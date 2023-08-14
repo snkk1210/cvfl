ab062f69705c54828b541d220421f99ecfbcef0e
snkk1210 <n41210guitar@gmail.com>
2023-08-14 17:21:49 +0000

    tmp4

--- main.py
+++ main.py
@@ -14,19 +14,19 @@ def lambda_handler(event=None, context=None):
 @app.route('/exec', methods=['POST'])
 def exec():
   
-	certificate_fs = flask.request.files['certificate']
-	certificate_fs.save('./certificate.crt')
-	privatekey_fs = flask.request.files['privatekey']
-	privatekey_fs.save('./privatekey.key')
-	intermediate_fs = flask.request.files['intermediate']
-	intermediate_fs.save('./intermediate.ca')
+	cert_fs = flask.request.files['cert']
+	cert_fs.save('/tmp/cert.pem')
+	privkey_fs = flask.request.files['privkey']
+	privkey_fs.save('/tmp/privkey.pem')
+	chain_fs = flask.request.files['chain']
+	chain_fs.save('/tmp/chain.pem')
 
-	res = subprocess.run('./app/cert_check.sh ./certificate.crt ./privatekey.key ./intermediate.ca', shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+	res = subprocess.run('./app/cert_check.sh /tmp/cert.pem /tmp/privkey.pem /tmp/chain.pem', shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 
 	# 一時ファイルを削除
-	#for tmpfile in glob.glob('./*'):
+	#for tmpfile in glob.glob('/tmp/*'):
 	#	os.remove(tmpfile)
-	#for crtfile in glob.glob('./*'):
+	#for crtfile in glob.glob('/tmp/*'):
 	#	os.remove(crtfile)
 
 	#return res.stdout.decode("utf8")
